f2613df38e5770e20fc50dfd9321fd8eb231ff77
Davi Garrocho <115951422+DaviGarrocho@users.noreply.github.com>
2024-11-20 18:54:14 -0300

    2 videos de JavaScript
    que run ein, 7 horas agora, da tempo ate de descansar, que loucura

---
+++ exercicios/aula8/ex1.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade do seu carro e ${vel}Km/h`)
+if(vel > 60){
+    console.log('voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('dirija sempre usando cinto de segurança!')
