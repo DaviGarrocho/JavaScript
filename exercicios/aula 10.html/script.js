f2b9eef4d7bbfd9677b56069db3f1a66e6eaa625
Davi Garrocho <115951422+DaviGarrocho@users.noreply.github.com>
2024-11-21 17:18:38 -0300

    2 aulas so de exercicio de JavaScript
    wow

---
+++ exercicios/aula 10.html/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('img')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if(hora >= 0 && hora < 12){
+    img.style.backgroundImage = 'url(estilos/imagens/manha.jpg)';
+    document.body.style.backgroundColor = '#c09d6e';
+}else if(hora >= 12 && hora < 18){
+    img.style.backgroundImage = 'url(estilos/imagens/tarde.jpg)';
+    document.body.style.backgroundColor = '#cf6339';
+    
+}else{
+    img.style.backgroundImage = 'url(estilos/imagens/noite.jpg)';
+    document.body.style.backgroundColor = '#071047';
+}
+
+}
+
